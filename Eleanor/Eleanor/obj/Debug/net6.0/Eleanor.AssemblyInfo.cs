c13e400b35d2ff5547e594668a96ad9e0a1114d4
StrojvedouciDenis <den.ti@seznam.cz>
2025-06-24 15:51:46 +0200

    ## version 1.0.0-alpha.27 (24.6.2025)
    * Multi-device development possibility tests
    * Added few dialogues
    * Preparation for 0.9.5 update

--- Eleanor/Eleanor/obj/Debug/net6.0/Eleanor.AssemblyInfo.cs
+++ Eleanor/Eleanor/obj/Debug/net6.0/Eleanor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Eleanor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0-alpha.26+0c97baab3403b6f46277c8ffbbf3b4b22d18faf5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0-alpha.26+247bca309534fa015488ff3d89b280d577b2275e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Eleanor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Eleanor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
